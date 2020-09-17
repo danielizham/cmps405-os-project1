a0d92371aa638a583787da4669f3ccda3872fb7a
Ali Moh <63345730+ali-am1807939@users.noreply.github.com>
2020-09-17 22:13:57 +0300

    Add basic commands

--- FileSystem.sh
+++ FileSystem.sh
@@ -7,4 +7,34 @@
 # Instructor : Heba D. M. Dawoud
 
 # for testing purposes
-echo TESTING: FileSystem.sh was called!
+
+cat /dev/null > OUTFILE.txt
+cat /dev/null > HOLDFILE.txt
+
+function sub1() {
+  echo "Date/Time of Search: $(date)" >> OUTFILE.txt
+}
+
+function sub2() {
+  echo "Searching for Files Larger Than 8Mb starting in $HOME"
+  echo "Please Standby for the Search Results..."
+  find ~ -size +8M 2>/dev/null > HOLDFILE.txt
+
+  if [[ -z "$(wc -c < HOLDFILE.txt)" ]]; then
+    echo "No files were found that are larger than 8MB"
+    echo "Exiting..."
+  else
+    cat HOLDFILE.txt >> OUTFILE.txt
+    echo "Number of files found: $(wc -l < HOLDFILE.txt)" >> OUTFILE.txt
+  fi
+}
+
+function sub3() {
+  echo "These search results are stored in $HOME/OUTFILE.txt"
+  echo "Search complete...Exiting..."
+  cat OUTFILE.txt
+}
+
+sub1
+sub2
+sub3
