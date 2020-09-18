f1a3fa651763d89a910d9aeb41659cb3cf99f571
Daniel Izham <daniel.izham@gmail.com>
2020-09-18 21:10:23 +0300

    Finish first subroutine

--- Performance.sh
+++ Performance.sh
@@ -8,3 +8,17 @@
 
 # for testing purposes
 echo TESTING: Performance.sh was called!
+
+create_stats() {
+	du --max-depth=1 --human-readable ~ | sort --human-numeric-sort --reverse --output=Disk_Usage.txt
+	dmesg -H > Message_Log.txt
+	cat /proc/cpuinfo > cpu_inf.txt
+	cat Message_Log.txt | wc -w > Message_Count.txt
+	tar zcf Phase1.tar.gz Disk_Usage.txt cpu_inf.txt Message_Count.txt
+	local time_now="$(date +"%H%M%S")"
+	mkdir "$time_now"
+	mv Phase1.tar.gz "$time_now"/
+}
+
+create_stats
+
