f22e5cff8a6c8df8bb4478f50bcdf22876207f73
Daniel Izham <daniel.izham@gmail.com>
2020-09-25 17:10:51 +0300

    Finish testing of script and make syntax corrections

--- Performance.sh
+++ Performance.sh
@@ -6,11 +6,11 @@
 # Assignment : Project 1
 # Instructor : Heba D. M. Dawoud
 
+# This subroutine backs up information about the current
+# status of this computer and archives it
 create_stats() {
-	# this subroutine backs up information about the current
-	# status of this computer and archives it
-
-	du --max-depth=1 --human-readable ~ | sort --human-numeric-sort --reverse --output=Disk_Usage.txt
+	du --max-depth=1 --human-readable ~ \
+		| sort --human-numeric-sort --reverse --output=Disk_Usage.txt
 	dmesg -H > Message_Log.txt
 	cat /proc/cpuinfo > cpu_inf.txt
 	cat Message_Log.txt | wc -w > Message_Count.txt
@@ -20,44 +20,55 @@ create_stats() {
 	cp Phase1.tar.gz "$time_now"/
 }
 
+# This subroutine makes a copy of each file in the home
+# directory with a permission u=rw in a backup directory.
+# Then, their permissions are modified to u=r.
 backup_rw_files() {
-	# this subroutine makes a copy of each file in the home
-	# directory with a permission u=rw in a backup directory.
-	# Then, their permissions are modified to u=r.
-
 	date_time_now="$(date +"%y%m%d%H%M%S")"
 	mkdir "$date_time_now"
-	find ~ -type f \                 # find all files only
-		\( -perm -600 \) \       # that have at least 600
+	find ~ -type f \
+		\( -perm -600 \) \
 		-and \
-		\( -not -perm -700 \) \  # but not more than 700
+		\( -not -perm -700 \) \
+		-exec cp '{}' "$date_time_now" ';' \
+	        2> /dev/null	
+		# find all files only
+	        # that have at least 600
+		# but not more than 700
 		# then copy them to the newly created directory
-		-exec cp '{}' "$date_time_now"/ +  
 	
 	# for this directory and its content, change only the owner
 	# permission with rw to r (igore the group and others).
-	find "$date_time_now" \          # find all dirs and files
-		\( -perm -600 \) \       # that have at least 600
+	find "$date_time_now" \
+		\( -perm -600 \) \
 		-and \
-		\( -not -perm -700 \) \  # but not more than 700
-		-exec chmod u=r '{}' +	 # change owner perm to r
+		\( -not -perm -700 \) \
+		-exec chmod u=r '{}' +
+		# find all dirs and files
+	        # that have at least 600
+		# but not more than 700
+		# change owner perm to r
 }
 
+# This subroutine displays to the console for the user
+# the conclusion of the first two subroutines.
 display_info() {
-	# this subroutine displays to the console for the user
-	# the conclusion of the first two subroutines.
-
 	# count only files that have u=r (do not count dirs 
 	# nor files with u=-, u=rwx, u=rw, u=wx, u=w or u=x).
 	echo -n "The number of files with u=r in dir "$date_time_now"/: "
-	find "$date_time_now" -type f \  # find only files
-		\( -perm -400 \) \	 # that have at least 400
+	find "$date_time_now" -type f \
+		\( -perm -400 \) \
 		-and \
-		\( -not -perm -600 \) \  # but not more than 600
-		| wc -l                  # then count them
+		\( -not -perm -600 \) \
+		| wc -l
+		# find only files
+		# that have at least 400
+		# but not more than 600
+		# then count them
 	echo "Backing up performance data has completed. Exiting..."
 }
 
+# Call all subroutines
 create_stats
 backup_rw_files
 display_info
