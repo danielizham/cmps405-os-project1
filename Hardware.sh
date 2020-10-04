5fbf1d95022acc5a50b28aa4e3dd712d79b496ef
Daniel Izham <daniel.izham@gmail.com>
2020-10-04 19:30:34 +0300

    Correct indentations (tabs => spaces)

--- Hardware.sh
+++ Hardware.sh
@@ -9,14 +9,14 @@
 # This subroutine outputs to stdout and the check.txt file
 # the used and free memory values in percentages.
 disp_mem() {
-	printf "Used Memory\tFree Memory\n" | tee -a check.txt
-	for t in $(seq 1 60); do
-		mem_info=$(free)
-		used=$(echo "$mem_info" | grep Mem | awk '{print $3/$2 * 100.0}')
-		free=$(echo "$mem_info" | grep Mem | awk '{print $4/$2 * 100.0}')
-		printf "%.2f%%\t\t%.2f%%\n" $used $free | tee -a check.txt
-		sleep 1s
-	done
+    printf "Used Memory\tFree Memory\n" | tee -a check.txt
+    for t in $(seq 1 60); do
+        mem_info=$(free)
+        used=$(echo "$mem_info" | grep Mem | awk '{print $3/$2 * 100.0}')
+        free=$(echo "$mem_info" | grep Mem | awk '{print $4/$2 * 100.0}')
+        printf "%.2f%%\t\t%.2f%%\n" $used $free | tee -a check.txt
+        sleep 1s
+    done
 }
 
 disp_mem
