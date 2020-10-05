b17299c85875d2272157b08d04df358a4bb9789c
Daniel Izham <daniel.izham@gmail.com>
2020-10-05 09:17:11 +0300

    Remove repetitions in the code

--- Hardware.sh
+++ Hardware.sh
@@ -12,9 +12,9 @@
 disp_mem() {
     printf "Used Memory\tFree Memory\n" | tee -a check.txt
     for t in $(seq 1 60); do
-        mem_info=$(free)
-        used=$(echo "$mem_info" | grep Mem | awk '{print $3/$2 * 100.0}')
-        free=$(echo "$mem_info" | grep Mem | awk '{print $4/$2 * 100.0}')
+        mem_info=$(free | grep Mem)
+        used=$(echo "$mem_info" | awk '{print $3/$2 * 100.0}')
+        free=$(echo "$mem_info" | awk '{print $4/$2 * 100.0}')
         printf "%.2f%%\t\t%.2f%%\n" $used $free | tee -a check.txt
         sleep 1s
     done
