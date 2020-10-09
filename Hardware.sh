658a1c5053b5e6c147b4af2deccb946f87e50a02
Daniel Izham <daniel.izham@gmail.com>
2020-10-09 19:51:23 +0300

    Add more comments within the subroutine

--- Hardware.sh
+++ Hardware.sh
@@ -6,18 +6,27 @@
 # Assignment : Project 1
 # Instructor : Heba D. M. Dawoud
 
-# This subroutine outputs to stdout and the check.txt file
-# the percentages of used and free memory every second for
-# one minute.
+# This subroutine outputs to stdout and the
+# check.txt file the percentages of used and
+# free memory every second for one minute.
 disp_mem() {
+    # Output header
     printf "Used Memory\tFree Memory\n" | tee -a check.txt
-    for t in $(seq 1 60); do
-        mem_info=$(free | grep Mem)
+
+    for t in $(seq 1 60); do # Loop 60 times 1 sec apart (1 min total) 
+	# Extract the line about the memory info from the free command
+        mem_info=$(free | grep Mem) 
+
+	# Divide the used and free values respectively by the total
+	# and multiply by 100 to convert to percentage
         used=$(echo "$mem_info" | awk '{print $3/$2 * 100.0}')
         free=$(echo "$mem_info" | awk '{print $4/$2 * 100.0}')
+
+	# Print out the percentages every second
         printf "%.2f%%\t\t%.2f%%\n" $used $free | tee -a check.txt
         sleep 1s
     done
 }
 
+# Call the subroutine
 disp_mem
